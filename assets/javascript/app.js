cf6d2998e4b42f0d971dba6e4143c940eef16407
LinaElkins <linali0925@gmail.com>
2018-03-22 20:36:43 -0400

    clocking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,23 +2,31 @@ var timer = 30;
 //Set timer  - imcomplete
 var intervalId;
 
-$("#play").on("click", run);
+$(document).ready(function() {
+$(".play").on("click", function (e) 
+{   e.preventDefault();
+    console.log("running");
+    run();
+});
+
+});
 
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
 
-    //  The decrement function.
-    function decrement() {
+//  The decrement function.
 
-        //  Decrease number by one.
-        timer--;
-  
-        //  Show the number in the #show-number tag.
-        $("#timer").html("<h2>" + timer + "</h2>");
-    };
-    
+function decrement() {
+
+    //  Decrease number by one.
+    timer--;
+
+    //  Show the number in the #show-number tag.
+    $("#timer").html("<h2>" + timer + "</h2>");
+};
+
 //Hide the question
 
 //if Timer runs out of time, it stops the game;
@@ -36,26 +44,26 @@ function submitAnswers() {
     var q6 = document.forms["quizForm"]["q6"].value;
 
     //Set our question loop
-   
 
-    for(i=1; i<=totalQ; i++) {
-        if(eval('q'+i) == null || eval('q'+i) =='') {
-            alert(' You missed question  ' +  i);
+
+    for (i = 1; i <= totalQ; i++) {
+        if (eval('q' + i) == null || eval('q' + i) == '') {
+            alert(' You missed question  ' + i);
             return false;
         }
     }
 
     //Correct answers for the questions
-    var answers = ['b','b','b','d','a','c'];
-    
+    var answers = ['b', 'b', 'b', 'd', 'a', 'c'];
+
     //Player gets 1 point for answering 1 question correctly
-    for(i=1; i<=totalQ; i++) {
-        if(eval('q'+i)== answers[i-1]) {
+    for (i = 1; i <= totalQ; i++) {
+        if (eval('q' + i) == answers[i - 1]) {
             score++;
         }
     }
     //display results after player submit answers
-    alert(" you scored  "  +  score + "  out of  " + totalQ);
+    alert(" you scored  " + score + "  out of  " + totalQ);
 
     return false;
 }
